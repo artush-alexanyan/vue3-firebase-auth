1138b1ed8739adba3fd65ac7ed0ee2158d6f392b
artush-alexanyan <artush.alexanyan1989@gmail.com>
2025-04-14 08:39:59 +0400

    Reseting default vuejs project

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  server: {
+    port: 8000,
+    open: true
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
